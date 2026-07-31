6d418f977d2464050fb49216836e24816dde162d
KrushanPatel <krushanpatel2@gmail.com>
2026-07-31 15:56:28 +0000

    initialize FastAPI project with index endpoint

---
+++ src/main.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI 
+import uvicorn
+
+app = FastAPI()
+
+@app.get('/')
+@app.get('/home')
+async def home():
+    return {
+            'user':'root',
+            'message':'Welcome to auth service'
+            }
+
+@app.get("/health")
+async def health():
+    return {"status": "ok"}
+
+
+if __name__ == "__main__":
+    print("auth-service starts...")
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
